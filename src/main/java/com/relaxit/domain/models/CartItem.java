2e1cc897a37ab4c3e4d23a3fbb786b315bb6e8e1
Hazim Karam <hazimkaram159@gmail.com>
2025-03-27 16:58:38 +0200

    Add CartItem Model

---
+++ src/main/java/com/relaxit/domain/models/CartItem.java
@@ -0,0 +1,57 @@
+package com.relaxit.domain.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class CartItem implements Serializable {
+
+    private Long cartId;
+    private Long userId;
+    private Long productId;
+    private int quantity = 1;
+    private LocalDateTime addedAt;
+
+    public CartItem() {
+    }
+
+    public Long getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(Long cartId) {
+        this.cartId = cartId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public LocalDateTime getAddedAt() {
+        return addedAt;
+    }
+
+    public void setAddedAt(LocalDateTime addedAt) {
+        this.addedAt = addedAt;
+    }
+
+}
