12d4609c8a760983849112c5653e44157214cbec
Hazim Karam <hazimkaram159@gmail.com>
2025-03-27 16:56:58 +0200

    Update Category model

--- src/main/java/com/relaxit/domain/models/Category.java
+++ src/main/java/com/relaxit/domain/models/Category.java
@@ -1,5 +1,39 @@
 package com.relaxit.domain.models;
 
-public class Category {
-    
+import java.io.Serializable;
+
+public class Category implements Serializable {
+
+    private Long categoryId;
+    private String name;
+    private String description;
+
+    public Category() {
+
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
