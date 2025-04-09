f411bde48589501f4f9e0d04beeb719be49b02bd
Hazim Karam <hazimkaram159@gmail.com>
2025-04-09 16:23:38 +0200

    Refactor UserRepository interface: add methods for add, update, delete, find by ID, and check existence by email

--- src/main/java/com/relaxit/repository/interfaces/UserRepository.java
+++ src/main/java/com/relaxit/repository/interfaces/UserRepository.java
@@ -1,8 +1,18 @@
 package com.relaxit.repository.interfaces;
 
 import com.relaxit.domain.models.User;
+import java.util.List;
 
 public interface UserRepository {
-    void save(User user);
+    void addUser(User user); 
+    void updateUser(User user);
+
     User findByEmail(String email);
+
+    User findById(Long userId);
+    void deleteById(Long userId);
+    List<User> getAllUsers();
+    boolean existsByEmail(String email);
+   
+
 }
