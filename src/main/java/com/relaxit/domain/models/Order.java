5a975740f4ca58a0852c117728659f60af7f1fec
mai <mainasser2014@gmail.com>
2025-04-10 12:57:16 +0200

    createDB

--- src/main/java/com/relaxit/domain/models/Order.java
+++ src/main/java/com/relaxit/domain/models/Order.java
@@ -1,21 +1,45 @@
 package com.relaxit.domain.models;
 
 import com.relaxit.domain.enums.OrderStatus;
-
+import jakarta.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "orders")
 public class Order implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
     private Long orderId;
-    private User user;
-    private LocalDateTime orderDate;
-    private BigDecimal totalAmount;
-    private OrderStatus status;
-    private List<OrderItem> items;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "order_date", updatable = false)
+    private LocalDateTime orderDate;
+
+    @Column(name = "total_amount", nullable = false, precision = 10, scale = 2)
+    private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private OrderStatus status = OrderStatus.PENDING;
+    
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    
+    @PrePersist
+    protected void onCreate() {
+        this.orderDate = LocalDateTime.now();
+    }
+    
     public Order() {
         this.orderDate = LocalDateTime.now();
         this.status = OrderStatus.PENDING;
@@ -61,11 +85,12 @@ public class Order implements Serializable {
         this.status = status;
     }
 
-    public List<OrderItem> getItems() {
-        return items;
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
     }
 
-    public void setItems(List<OrderItem> items) {
-        this.items = items;
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
     }
+
 }
---
+++ src/main/java/com/relaxit/domain/utils/JPAUtil.java
@@ -0,0 +1,30 @@
+package com.relaxit.domain.utils;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.relaxit.domain.enums.UserRole;
+import com.relaxit.domain.models.*;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JPAUtil {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("myJPAUnit");
+    
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return emf;
+    }
+    
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+    
+    public static void close() {
+        emf.close();
+    }
+
+}
