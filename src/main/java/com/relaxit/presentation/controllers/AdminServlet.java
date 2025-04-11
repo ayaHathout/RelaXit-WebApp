56a2e86a8be3a0c3c65add5fec32d82c7f402612
Hazim Karam <hazimkaram159@gmail.com>
2025-04-11 12:54:08 +0200

    add AdminServlet

---
+++ src/main/java/com/relaxit/presentation/controllers/AdminServlet.java
@@ -0,0 +1,30 @@
+package com.relaxit.presentation.controllers;
+
+import com.relaxit.domain.models.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import com.relaxit.domain.enums.UserRole;
+
+import java.io.IOException;
+
+public class AdminServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null && session.getAttribute("user") != null) {
+            User user = (User) session.getAttribute("user");
+            if (user.getRole() == UserRole.ADMIN) {
+                request.getRequestDispatcher("/views/thankyou.jsp").forward(request, response);
+            } else {
+                response.sendRedirect(request.getContextPath() + "/login");
+            }
+        } else {
+            response.sendRedirect(request.getContextPath() + "/login");
+        }
+    }
+}
