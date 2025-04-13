6f6a8ee1f7e597f16b288dd6f0ed9fe2be6d713d
Hazim Karam <hazimkaram159@gmail.com>
2025-04-13 15:12:17 +0200

    add UserRepository.java interface

--- src/main/java/com/relaxit/repository/Interfaces/UserRepository.java
+++ src/main/java/com/relaxit/repository/Interfaces/UserRepository.java
@@ -1,8 +1,18 @@
 package com.relaxit.repository.Interfaces;
 
 import com.relaxit.domain.models.User;
+import java.util.List;
 
 public interface UserRepository {
+    void addUser(User user); 
+    void updateUser(User user);
 
-    User findById(Long id);
+    User findByEmail(String email);
+
+    User findById(Long userId);
+    void deleteById(Long userId);
+    List<User> getAllUsers();
+    boolean existsByEmail(String email);
+   
+
 }
