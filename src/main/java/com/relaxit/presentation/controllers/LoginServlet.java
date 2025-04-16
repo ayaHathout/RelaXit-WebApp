bea63403efb9f169cfd76794f468d65579c3698e
Hazim Karam <hazimkaram159@gmail.com>
2025-04-17 00:49:44 +0200

    update LoginServlet

--- src/main/java/com/relaxit/presentation/controllers/LoginServlet.java
+++ src/main/java/com/relaxit/presentation/controllers/LoginServlet.java
@@ -1,10 +1,7 @@
 package com.relaxit.presentation.controllers;
 
-import com.relaxit.domain.models.CartItem;
 import com.relaxit.domain.models.User;
-import com.relaxit.domain.services.CartService;
 import com.relaxit.domain.services.UserService;
-import com.relaxit.presentation.controllers.CartServlet;
 import com.relaxit.repository.Impl.UserRepositoryImpl;
 import com.relaxit.domain.enums.UserRole;
 
@@ -15,18 +12,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class LoginServlet extends HttpServlet {
 
     private UserService userService;
-   private CartService cartService;
 
     @Override
     public void init() throws ServletException {
         userService = new UserService(new UserRepositoryImpl());
-        cartService = new CartService();
     }
 
     @Override
@@ -47,18 +40,13 @@ public class LoginServlet extends HttpServlet {
                 HttpSession session = request.getSession();
                 session.setAttribute("user", user);
 
-                // Merge session cart with user cart
-                List<CartItem> sessionCart = (List<CartItem>) session.getAttribute("sessionCart");
-                if (sessionCart != null && !sessionCart.isEmpty()) {
-                    cartService.mergeSessionCartWithUserCart(user.getUserId(), sessionCart);
-                    session.setAttribute("sessionCart", new ArrayList<CartItem>());
-                }
-
                 // Check user role and redirect accordingly
                 if (user.getRole() == UserRole.ADMIN) {
-                    response.sendRedirect(request.getContextPath() + "/thankyou"); // Assuming /thankyou is mapped to AdminServlet
+                    response.sendRedirect(request.getContextPath() + "/admin/users"); 
+                    // admin/users
                 } else if (user.getRole() == UserRole.USER) {
-                    response.sendRedirect(request.getContextPath() + "/home"); // Mapped to ProfileServlet
+                    response.sendRedirect(request.getContextPath() + "/profile"); 
+                    // home
                 } else {
                     request.setAttribute("error", "Unknown user role");
                     request.getRequestDispatcher("/views/login.jsp").forward(request, response);
